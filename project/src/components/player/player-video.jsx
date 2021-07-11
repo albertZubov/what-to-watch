c976764c27de49eb6149451bba5cc07bcd5faf49
Johnny Rocket <0.snilcy@gmail.com>
2021-07-11 20:11:35 +0300

    Доработка компонента Видеоплеер

--- project/src/components/player/player-video.jsx
+++ project/src/components/player/player-video.jsx
@@ -1,24 +1,21 @@
 import React from 'react'
-import { getFilm } from '../../store/selectors'
 import { propFilm } from '../../props/props'
 import PropTypes from 'prop-types'
-import { connect } from 'react-redux'
 
 const PlayerVideo = (props) => {
-	const { film } = props
-	const { videoLink } = film
+	const { film, isPlaying } = props
+	const { previewVideoLink, posterImage } = film
 	const { refLink, onTimeUpdate, onDurationChange } = props
 
 	return (
 		<video
-			src={videoLink}
+			src={previewVideoLink}
 			className='player__video'
-			poster='img/player-poster.jpg'
+			poster={posterImage}
 			ref={refLink}
 			onTimeUpdate={onTimeUpdate}
 			onDurationChange={onDurationChange}
-			preload='auto'
-			autoPlay
+			autoPlay={isPlaying}
 			muted
 		></video>
 	)
@@ -29,10 +26,7 @@ PlayerVideo.propTypes = {
 	onTimeUpdate: PropTypes.func,
 	onDurationChange: PropTypes.func,
 	refLink: PropTypes.object,
+	isPlaying: PropTypes.bool,
 }
 
-const mapStateToProps = (state, { activeId }) => ({
-	film: getFilm(state, activeId),
-})
-
-export default connect(mapStateToProps)(PlayerVideo)
+export default PlayerVideo
