752c735899fdb201bc695afb61acff24cd826728
Albert <zubov.warrior@gmail.com>
2022-07-04 09:13:21 +0300

    MainPage, realization search

--- project/src/components/film-list/film-list.tsx
+++ project/src/components/film-list/film-list.tsx
@@ -1,22 +1,26 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { connect } from 'react-redux'
 import FilmCardPreview from '../film-card-preview/film-card-preview'
 import { getGenresFilms } from '../../store/selectors'
 import ButtonShowMore from '../button-show-more/button-show-more'
 import { FilmType, StateType } from '../../types/types'
+import './film-list.css'
 
 const DEFAULT_FILMS = 8
 const ADD_FILMS = 8
 
 const FilmList = ({ filmsOnGenre }: { filmsOnGenre: FilmType[] }) => {
 	const [quantity, setQuantity] = useState(DEFAULT_FILMS)
+	const [search, setSearch] = useState('')
+
+	const films = filmsOnGenre.filter((el: any) =>
+		el.name.toLowerCase().includes(search)
+	)
 
 	const filmsArr =
-		filmsOnGenre.length > DEFAULT_FILMS
-			? filmsOnGenre.slice(0, quantity)
-			: filmsOnGenre
+		films.length > DEFAULT_FILMS ? films.slice(0, quantity) : films
 	const showButton =
-		filmsOnGenre.length > DEFAULT_FILMS && filmsOnGenre.length >= quantity ? (
+		films.length > DEFAULT_FILMS && films.length >= quantity ? (
 			<ButtonShowMore onClickButton={() => setQuantity(quantity + ADD_FILMS)} />
 		) : (
 			''
@@ -24,8 +28,16 @@ const FilmList = ({ filmsOnGenre }: { filmsOnGenre: FilmType[] }) => {
 
 	return (
 		<>
+			<form className='search'>
+				<input
+					onInput={({ target }: any) => setSearch(target.value)}
+					type='text'
+					placeholder='Название фильма'
+					className='search__input input'
+				/>
+			</form>
 			<div className='catalog__films-list'>
-				{filmsArr.map((film) => (
+				{filmsArr.map((film: any) => (
 					<FilmCardPreview film={film} key={film.id} />
 				))}
 			</div>
